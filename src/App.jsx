bb906c6e7f632247fce735ba8e025e48fb44beff
ydctnr <152324574+ydctnr@users.noreply.github.com>
2024-09-02 19:55:36 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
 
 
-function App() {
-  return(
-    <h1 className="text-3xl font-bold underline">
-       Hello world!
-    </h1>
-  )
+const App = () => {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header></Header>
+        <Hero></Hero>
+        <Benefits></Benefits>
+        <Collaboration></Collaboration>
+        <Services></Services>
+        <Pricing></Pricing>
+        <Roadmap></Roadmap>
+        <Footer></Footer>
+        
+      </div>
 
-}
+      <ButtonGradient />
 
-export default App
+    </>
+  );
+};
+
+export default App;
